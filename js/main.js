1fde6f1adb9678eb7cb726da1c8593f9b7941087
ginnyli <meiqing.virginia.li@gmail.com>
2018-05-05 17:54:51 -0400

    2

--- js/main.js
+++ js/main.js
@@ -15,56 +15,224 @@ var map = new mapboxgl.Map({
   zoom: 11
 });
 
-var data_SFroads = "https://raw.githubusercontent.com/meiqingli/CPLN692-Final/data/2013SF_roads.geojson";
-var data_CMP = "https://raw.githubusercontent.com/meiqingli/CPLN692-Final/master/data/road.geojson";
-var SFroads = $ajax(data_SFroads);
-var CMProads = $ajax(data_CMP);
+var data_SFroads = "https://gist.githubusercontent.com/meiqingli/d9de32bd2ffa2194303f731e67d2cd9d/raw/cee8a44a6c6d29e7aa04f3bc851a289c0580e9ef/SFroads";
+var data_CMP = "https://gist.githubusercontent.com/meiqingli/d9de32bd2ffa2194303f731e67d2cd9d/raw/cee8a44a6c6d29e7aa04f3bc851a289c0580e9ef/SFCMP";
+var featureGroup;
 
-map.on('load', function() {
-  map.addLayer({
-    id: 'SFroads',
-    type: 'line',
-    source: {
-      type: 'geojson',
-      data: SFroads
-    },
-    layout: {
-      "line-join": "round",
-      "line-cap": "round"
-    },
-    paint: {
-      "line-color": "#888",
-      "line-width": 3
-    }
-  });
-  map.addLayer({
-    id: 'CMProads',
-    type: 'line',
-    source: {
-      type: 'geojson',
-      data: CMProads
-    },
-    layout: {
-      "line-join": "round",
-      "line-cap": "round"
-    },
-    paint: {
-      "line-color": "#888",
-      "line-width": 5
-    }
+var myStyle = function(feature) {
+  if (feature.properties.cls_hcm00 == "1")
+  {return {color: '#8DD3C7'};}
+  else if (feature.properties.cls_hcm00 == "2")
+  {return {color: '#FFFFB3'};}
+  else if (feature.properties.cls_hcm00 == "3")
+  {return {color: '#BEBADA'};}
+  else if (feature.properties.cls_hcm00 == "4")
+  {return {color: '#FB8072'};}
+  else if (feature.properties.cls_hcm00 == "Fwy")
+  {return {color: '#80B1D3'};}
+};
+
+var showResults = function() {
+  $('#intro').hide();
+  $('#results').show();
+};
+
+var Filter = function(feature) {
+  return true;};
+
+var Filter1 = function(feature) {
+  if (feature.properties.cls_hcm00 == "1"){return true;}
+  else {return false;}
+};
+
+var Filter2 = function(feature) {
+  if (feature.properties.cls_hcm00 == "2"){return true;}
+  else {return false;}
+};
+
+var Filter3 = function(feature) {
+  if (feature.properties.cls_hcm00 == "3"){return true;}
+  else {return false;}
+};
+
+var Filter4 = function(feature) {
+  if (feature.properties.cls_hcm00 == "4"){return true;}
+  else {return false;}
+};
+
+var freewayFilter = function(feature) {
+  if (feature.properties.cls_hcm00 == "Fwy"){return true;}
+  else {return false;}
+};
+
+var myFilter = Filter;
+
+$(document).ready(function() {
+  $.ajax(SFRoads).done(function(data) {
+    var parsedData = JSON.parse(data);
+    featureGroup = L.geoJson(parsedData, {
+      style: myStyle,
+      onEachFeature: function(feature,layer){
+        layer.bindPopup(feature.properties.cmp_name);
+        layer.on('mouseover', function(event){
+        layer.openPopup();});
+        layer.on('mouseout', function(event){
+        layer.closePopup();});
+      },
+      filter: myFilter,
+    }).addTo(map);
   });
 });
 
-// var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
-//   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-//   subdomains: 'abcd',
-//   minZoom: 0,
-//   maxZoom: 20,
-//   ext: 'png'
-// }).addTo(map);
+// $('#next').click(function(event){
+// map.removeLayer(featureGroup);
+//  if (countPage == 1){
+//   myFilter = Filter1;
+//   streettype = "Urban Street I (High Speed)";
+//   text = "driveways of very low density without parking. These streets are multilane divided, undivided or two-lane with shoulders. Speed limit for this type of streets usually ranges from 75 to 90 km/h. Since they are located in vary low density areas, there are very little pedestrian activity and roadside development.";
+//  }
+//  if (countPage == 2){
+//   myFilter = Filter2;
+//   streettype = "Urban Street II (Suburban)";
+//   text="driveways of low density without parking. These streets are multilane divided, undivided or two-lane with shoulders. Speed limit for this type of streets usually ranges from 65 to 75 km/h. Pedestrian activity is little, and there are often low to medium density roadside development. ";
+//  }
+//  if (countPage == 3){
+//   myFilter = Filter3;
+//   streettype = "Urban Street III (Intermediate)";
+//   text="driveways of moderate density with some parking. These streets are multilane divided or undivided, or one-way, two-lane. Speed limit for this type of streets usually ranges from 50 to 65 km/h. There are some pedestrian activities with medium to moderate density roadside development.";
+//  }
+//  if (countPage == 4){
+//   myFilter = Filter4;
+//   streettype = "Urban Street IV (Urban)";
+//   text="driveways of high density with significant parking. These streets are usually undivided one-way or two-way with two or more lanes. Speed limit for this type of streets usually ranges from 40 to 55 km/h. There are usually pedestrian activities with high density roadside development.";
+//  }
+//  if (countPage == 5){
+//   myFilter = freewayFilter;
+//   streettype = "Freeway";
+//   text="A freeway is defined as a divided highway facility with two or more lanes in each direction and full control of access and egress. It has no intersections; access and egress are provided by ramps at interchanges.";
+//  }
+//  $(".street-type").text(streettype);
+//  $(".description").text(text);
+//  showResults();
+//  $(document).ready(function() {
+//    $.ajax(dataset).done(function(data) {
+//      var parsedData = JSON.parse(data);
+//      featureGroup = L.geoJson(parsedData, {
+//        style: myStyle,
+//        onEachFeature: function(feature,layer){
+//          layer.bindPopup(feature.properties.cmp_name);
+//          layer.on('mouseover', function(event){
+//          layer.openPopup();});
+//          layer.on('mouseout', function(event){
+//          layer.closePopup();});
+//        },
+//        filter: myFilter,
+//      }).addTo(map);
+//    });
+//  });
+// });
 
-// Add Mapbox layer to map
+// $('#previous').click(function(){
+//   countPage --;
+//   $('#next').show();
+//   map.removeLayer(featureGroup);
+//    if (countPage == 1){
+//     myFilter = Filter1;
+//     streettype = "Urban Street I (High Speed)";
+//     text = "driveways of very low density without parking. These streets are multilane divided, undivided or two-lane with shoulders. Speed limit for this type of streets usually ranges from 75 to 90 km/h. Since they are located in vary low density areas, there are very little pedestrian activity and roadside development.";
+//     document.getElementById("image").src="images/US1.png";
+//     $('#previous').hide();
+//    }
+//    if (countPage == 2){
+//     myFilter = Filter2;
+//     streettype = "Urban Street II (Suburban)";
+//     text="driveways of low density without parking. These streets are multilane divided, undivided or two-lane with shoulders. Speed limit for this type of streets usually ranges from 65 to 75 km/h. Pedestrian activity is little, and there are often low to medium density roadside development. ";
+//     document.getElementById("image").src="images/US2.png";
+//    }
+//    if (countPage == 3){
+//     myFilter = Filter3;
+//     streettype = "Urban Street III (Intermediate)";
+//     text="driveways of moderate density with some parking. These streets are multilane divided or undivided, or one-way, two-lane. Speed limit for this type of streets usually ranges from 50 to 65 km/h. There are some pedestrian activities with medium to moderate density roadside development.";
+//     document.getElementById("image").src="images/US3.png";
+//    }
+//    if (countPage == 4){
+//     myFilter = Filter4;
+//     streettype = "Urban Street IV (Urban)";
+//     text="driveways of high density with significant parking. These streets are usually undivided one-way or two-way with two or more lanes. Speed limit for this type of streets usually ranges from 40 to 55 km/h. There are usually pedestrian activities with high density roadside development.";
+//     document.getElementById("image").src="images/US4.png";
+//    }
+//    if (countPage == 5){
+//     myFilter = freewayFilter;
+//     streettype = "Freeway";
+//     text="A freeway is defined as a divided highway facility with two or more lanes in each direction and full control of access and egress. It has no intersections; access and egress are provided by ramps at interchanges.";
+//     document.getElementById("image").src="images/Freeway.png";
+//    }
+//    $(".street-type").text(streettype);
+//    $(".description").text(text);
+//    showResults();
+//    $(document).ready(function() {
+//      $.ajax(dataset).done(function(data) {
+//        var parsedData = JSON.parse(data);
+//        featureGroup = L.geoJson(parsedData, {
+//          style: myStyle,
+//          onEachFeature: function(feature,layer){
+//            layer.bindPopup(feature.properties.cmp_name);
+//            layer.on('mouseover', function(event){
+//            layer.openPopup();});
+//            layer.on('mouseout', function(event){
+//            layer.closePopup();});
+//          },
+//          filter: myFilter,
+//        }).addTo(map);
+//      });
+//    });
+// });
+
 // map.on('load', function() {
+//   // map.addLayer({
+//   //   id: 'SFroads',
+//   //   type: 'line',
+//   //   source: {
+//   //     type: 'geojson',
+//   //     data: SFroads
+//   //   },
+//   //   layout: {
+//   //     "line-join": "round",
+//   //     "line-cap": "round"
+//   //   },
+//   //   paint: {
+//   //     "line-color": "#888",
+//   //     "line-width": 3
+//   //   }
+//   // });
+//   map.addLayer({
+//     id: 'CMProads',
+//     type: 'line',
+//     source: {
+//       type: 'geojson',
+//       data: CMProads
+//     },
+//     layout: {
+//       "line-join": "round",
+//       "line-cap": "round"
+//     },
+//     paint: {
+//       "line-color": "#888",
+//       "line-width": 5
+//     }
+//   });
+// });
+//
+// // var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
+// //   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+// //   subdomains: 'abcd',
+// //   minZoom: 0,
+// //   maxZoom: 20,
+// //   ext: 'png'
+// // }).addTo(map);
+//
+// // Add Mapbox layer to map
+// // map.on('load', function() {
 //   map.addLayer({
 //     id: 'terrain-data',
 //     type: 'line',
