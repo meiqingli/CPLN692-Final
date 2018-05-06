7cc92584434622d0483e97569957b25795246fb4
ginnyli <meiqing.virginia.li@gmail.com>
2018-05-06 01:48:45 -0400

    CMP type filter

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+/* global $, mapboxgl */
 // constants
 var roadsUrl = 'https://gist.githubusercontent.com/meiqingli/29972740342cff8c3ab514750d3ab1fd/raw/382fbc90bed5a7e087b7e9d76c766960b5e52df5/SFroads';
 var cmpUrl = 'https://gist.githubusercontent.com/meiqingli/d9de32bd2ffa2194303f731e67d2cd9d/raw/cee8a44a6c6d29e7aa04f3bc851a289c0580e9ef/SFCMP';
+
+//filter CMP segments
+function getCMPColor(feature) {
+  if (feature.properties.cls_hcm00 === '1') { return { color: '#8DD3C7' }; }
+  else if (feature.properties.cls_hcm00 === '2') { return { color: '#FFFFB3' }; }
+  else if (feature.properties.cls_hcm00 === '3') { return { color: '#BEBADA' }; }
+  else if (feature.properties.cls_hcm00 === '4') { return { color: '#FB8072' }; }
+  else if (feature.properties.cls_hcm00 === 'Fwy') { return { color: '#80B1D3' }; }
+}
+
 
 async function doAjax(url) {
   let result;
@@ -8,6 +19,7 @@ async function doAjax(url) {
     result = await $.getJSON(url);
     return result;
   } catch (error) {
+    // eslint-disable-next-line
     console.error(error);
   }
 }
@@ -32,7 +44,7 @@ $(() => {
   // add layers when map is ready
   map.on('load', async function () {
     var cmpData = await doAjax(cmpUrl);
-    var roadsData = await doAjax(roadsUrl)
+    var roadsData = await doAjax(roadsUrl);
 
     map.addLayer({
       id: 'SFroads',
@@ -68,21 +80,29 @@ $(() => {
       }
     });
 
-    map.addLayer({
-      id: 'CMPtypes',
-      type: 'line',
-      source: {
-        type: 'geojson',
-        data: {
-          type: 'FeatureCollection',
-          features:[{
-            type: 'Feature',
-            properties: {
-              cls_hcm00: '1'
-            }
-          }]
-        },
+
+    // examples to filter the features and add source -> add layer
+    // filter the features
+    var HighSpeed = cmpData.features.filter(item => item.properties.cls_hcm00 === '1');
+    var Suburban = cmpData.features.filter(item => item.properties.cls_hcm00 === '2');
+    var Intermediate = cmpData.features.filter(item => item.properties.cls_hcm00 === '3');
+    var Urban = cmpData.features.filter(item => item.properties.cls_hcm00 === '4');
+    var Freeway = cmpData.features.filter(item => item.properties.cls_hcm00 === 'Fwy');
+
+    // add source
+    map.addSource('HighSpeed', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: HighSpeed,
       },
+    });
+
+    // add layer
+    map.addLayer({
+      id: 'HighSpeed',
+      type: 'line',
+      source: 'HighSpeed',
       layout: {
         'line-join': 'round',
         'line-cap': 'round'
@@ -93,163 +113,352 @@ $(() => {
       }
     });
 
+    // add source
+    map.addSource('Suburban', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: Suburban,
+      },
+    });
+
+    // add layer
+    map.addLayer({
+      id: 'Suburban',
+      type: 'line',
+      source: 'Suburban',
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#ffffb3',
+        'line-width': 3
+      }
+    });
+
+    // add source
+    map.addSource('Intermediate', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: Intermediate,
+      },
+    });
+
+    // add layer
+    map.addLayer({
+      id: 'Intermediate',
+      type: 'line',
+      source: 'Intermediate',
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#bebada',
+        'line-width': 3
+      }
+    });
+
+    // add source
+    map.addSource('Urban', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: Urban,
+      },
+    });
+
+    // add layer
+    map.addLayer({
+      id: 'Urban',
+      type: 'line',
+      source: 'Urban',
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#fb8072',
+        'line-width': 3
+      }
+    });
+
+    // add source
+    map.addSource('Freeway', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: Freeway,
+      },
+    });
+
+    // add layer
+    map.addLayer({
+      id: 'Freeway',
+      type: 'line',
+      source: 'Freeway',
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#80b1d3',
+        'line-width': 3
+      }
+    });
+
     //add popup of CMP names
     var popup = new mapboxgl.Popup();
-    map.on('mousemove', function(e) {
+    map.on('mousemove', function (e) {
       var features = map.queryRenderedFeatures(e.point, { layers: ['CMProads'] });
-      if (!features.length) {
+      // will return [], if no feature exist
+      if (features.length === 0) {
         popup.remove();
         return;
       }
       var feature = features[0];
 
       popup.setLngLat(feature.geometry.coordinates[0])
-      .setHTML(feature.properties.cmp_name)
-      .addTo(map);
+        .setHTML(feature.properties.cmp_name)
+        .addTo(map);
 
       map.getCanvas().style.cursor = features.length ? 'pointer' : '';
     });
+
+
+    // var showResults = function() {
+    //   $('#intro').hide();
+    //   $('#results').show();
+    // };
+
+    $('#HighSpeed').click(function (event) {
+      map.removeLayer('HighSpeed');
+      map.removeLayer('Suburban');
+      map.removeLayer('Intermediate');
+      map.removeLayer('Urban');
+      map.removeLayer('Freeway');
+      map.addLayer({
+        id: 'HighSpeed',
+        type: 'line',
+        source: 'HighSpeed',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#8DD3C7',
+          'line-width': 3
+        }
+      });
+    });
+
+    $('#Suburban').click(function (event) {
+      map.removeLayer('HighSpeed');
+      map.removeLayer('Suburban');
+      map.removeLayer('Intermediate');
+      map.removeLayer('Urban');
+      map.removeLayer('Freeway');
+      map.addLayer({
+        id: 'Suburban',
+        type: 'line',
+        source: 'Suburban',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#ffffb3',
+          'line-width': 3
+        }
+      });
+    });
+
+    $('#Intermediate').click(function (event) {
+      map.removeLayer('HighSpeed');
+      map.removeLayer('Suburban');
+      map.removeLayer('Intermediate');
+      map.removeLayer('Urban');
+      map.removeLayer('Freeway');
+      map.addLayer({
+        id: 'Intermediate',
+        type: 'line',
+        source: 'Intermediate',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#bebada',
+          'line-width': 3
+        }
+      });
+    });
+
+    $('#Urban').click(function (event) {
+      map.removeLayer('HighSpeed');
+      map.removeLayer('Suburban');
+      map.removeLayer('Intermediate');
+      map.removeLayer('Urban');
+      map.removeLayer('Freeway');
+      map.addLayer({
+        id: 'Urban',
+        type: 'line',
+        source: 'Urban',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#fb8072',
+          'line-width': 3
+        }
+      });
+    });
+
+    $('#Freeway').click(function (event) {
+      map.removeLayer('HighSpeed');
+      map.removeLayer('Suburban');
+      map.removeLayer('Intermediate');
+      map.removeLayer('Urban');
+      map.removeLayer('Freeway');
+      map.addLayer({
+        id: 'Freeway',
+        type: 'line',
+        source: 'Freeway',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#80b1d3',
+          'line-width': 3
+        }
+      });
+    });
+
+    $('#All').click(function (event) {
+      map.addLayer({
+        id: 'HighSpeed',
+        type: 'line',
+        source: 'HighSpeed',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#8DD3C7',
+          'line-width': 3
+        }
+    });
+      map.addLayer({
+        id: 'Suburban',
+        type: 'line',
+        source: 'Suburban',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#ffffb3',
+          'line-width': 3
+        }
+    });
+      map.addLayer({
+        id: 'Intermediate',
+        type: 'line',
+        source: 'Intermediate',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#bebada',
+          'line-width': 3
+        }
+    });
+      map.addLayer({
+        id: 'Urban',
+        type: 'line',
+        source: 'Urban',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#fb8072',
+          'line-width': 3
+        }
+    });
+      map.addLayer({
+        id: 'Freeway',
+        type: 'line',
+        source: 'Freeway',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#80b1d3',
+          'line-width': 3
+        }
+      });
+    });
+
+
+    // var cartoUserName = 'meiqingli';
+    //
+    // var myLayer;
+    //
+    // var segments = cartodb.createLayer(map, {
+    //   user_name: cartoUserName,
+    //   type: 'cartodb',
+    //   interactivity: true,
+    //   sublayers: [
+    //     {
+    //       sql: 'SELECT * FROM road',
+    //       cartocss: '#road { line-width: 1;line-color: #717975;line-opacity: 1;}',
+    //       interactivity: ['cls_hcm00', 'direction']
+    //    },
+    //     {
+    //        sql: 'SELECT * FROM road where cls_hcm00 = 'Fwy'',
+    //        cartocss: '#road {line-width: 2;line-color: #80b1d3;line-opacity: 1;}',
+    //        interactivity: ['cls_hcm00', 'direction']
+    //     },
+    //     {
+    //        sql: 'SELECT * FROM road where cls_hcm00 = '1'',
+    //        cartocss: '#road {line-width: 2;line-color: #8dd3c7;line-opacity: 1;}',
+    //        interactivity: ['cls_hcm00', 'direction']
+    //     },
+    //     {
+    //        sql: 'SELECT * FROM road where cls_hcm00 = '2'',
+    //        cartocss: '#road {line-width: 2;line-color: #ffffb3;line-opacity: 1;}',
+    //        interactivity: ['cls_hcm00', 'direction']
+    //     },
+    //     {
+    //        sql: 'SELECT * FROM road where cls_hcm00 = '3'',
+    //        cartocss: '#road {line-width: 2;line-color: #bebada;line-opacity: 1;}',
+    //        interactivity: ['cls_hcm00', 'direction']
+    //     },
+    //     {
+    //        sql: 'SELECT * FROM road where cls_hcm00 = '4'',
+    //        cartocss: '#road {line-width: 2;line-color: #fb8072;line-opacity: 1;}',
+    //        interactivity: ['cls_hcm00', 'direction']
+    //     },
+    //   ]
+    //
+    // }).addTo(map)
+    //   .on('done', function(layer) {
+    //     // Set interactivity
+    //     layer.setInteraction(true);
+    //     // Set up event
+    //     layer.on('featureClick',function(e, latlng, pos, data) {
+    //       console.log(data);
+    //     });
+    //   }).on('error', function() {
+    //     console.log('some error occurred');
+    // });
   });
 
-
-  //filter CMP segments
-  var myStyle = function(feature) {
-  if (feature.properties.cls_hcm00 == "1")
-  {return {color: '#8DD3C7'};}
-  else if (feature.properties.cls_hcm00 == "2")
-  {return {color: '#FFFFB3'};}
-  else if (feature.properties.cls_hcm00 == "3")
-  {return {color: '#BEBADA'};}
-  else if (feature.properties.cls_hcm00 == "4")
-  {return {color: '#FB8072'};}
-  else if (feature.properties.cls_hcm00 == "Fwy")
-  {return {color: '#80B1D3'};}
-};
-
-// var Filter = function(feature) {
-//   return true;};
-//
-// var Filter1 = function(feature) {
-//   if (feature.properties.cls_hcm00 == "1"){return true;}
-//   else {return false;}
-// };
-//
-// var Filter2 = function(feature) {
-//   if (feature.properties.cls_hcm00 == "2"){return true;}
-//   else {return false;}
-// };
-//
-// var Filter3 = function(feature) {
-//   if (feature.properties.cls_hcm00 == "3"){return true;}
-//   else {return false;}
-// };
-//
-// var Filter4 = function(feature) {
-//   if (feature.properties.cls_hcm00 == "4"){return true;}
-//   else {return false;}
-// };
-//
-// var freewayFilter = function(feature) {
-//   if (feature.properties.cls_hcm00 == "Fwy"){return true;}
-//   else {return false;}
-// };
-//
-// var myFilter = Filter;
-
-// $(document).ready(function() {
-//   $.ajax(cmpUrl).done(function(data) {
-//     var parsedData = JSON.parse(data);
-//     featureGroup = L.geoJson(parsedData, {
-//       style: myStyle,
-//       filter: myFilter,
-//     }).addTo(map);
-//   });
-// });
-
-// $('HighSpeed').click(function(event){
-//   myFilter = Filter1;
-// });
-//
-// $('Suburban').click(function(event){
-//   myFilter = Filter2;
-// });
-//
-// $('Intermediate').click(function(event){
-//   myFilter = Filter3;
-// });
-//
-// $('Urban').click(function(event){
-//   myFilter = Filter4;
-// });
-//
-// $('Freeway').click(function(event){
-//   myFilter = freewayFilter;
-// });
-// * Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-  function openNav() {
-    document.getElementById('mySidenav').style.width = '250px';
-    document.getElementById('main').style.marginLeft = '250px';
-    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
-  }
-
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-  function closeNav() {
-    document.getElementById('mySidenav').style.width = '0';
-    document.getElementById('main').style.marginLeft = '0';
-    document.body.style.backgroundColor = 'white';
-  }
-
-
-  // var cartoUserName = 'meiqingli';
-  //
-  // var myLayer;
-  //
-  // var segments = cartodb.createLayer(map, {
-  //   user_name: cartoUserName,
-  //   type: 'cartodb',
-  //   interactivity: true,
-  //   sublayers: [
-  //     {
-  //       sql: 'SELECT * FROM road',
-  //       cartocss: '#road { line-width: 1;line-color: #717975;line-opacity: 1;}',
-  //       interactivity: ['cls_hcm00', 'direction']
-  //    },
-  //     {
-  //        sql: 'SELECT * FROM road where cls_hcm00 = 'Fwy'',
-  //        cartocss: '#road {line-width: 2;line-color: #80b1d3;line-opacity: 1;}',
-  //        interactivity: ['cls_hcm00', 'direction']
-  //     },
-  //     {
-  //        sql: 'SELECT * FROM road where cls_hcm00 = '1'',
-  //        cartocss: '#road {line-width: 2;line-color: #8dd3c7;line-opacity: 1;}',
-  //        interactivity: ['cls_hcm00', 'direction']
-  //     },
-  //     {
-  //        sql: 'SELECT * FROM road where cls_hcm00 = '2'',
-  //        cartocss: '#road {line-width: 2;line-color: #ffffb3;line-opacity: 1;}',
-  //        interactivity: ['cls_hcm00', 'direction']
-  //     },
-  //     {
-  //        sql: 'SELECT * FROM road where cls_hcm00 = '3'',
-  //        cartocss: '#road {line-width: 2;line-color: #bebada;line-opacity: 1;}',
-  //        interactivity: ['cls_hcm00', 'direction']
-  //     },
-  //     {
-  //        sql: 'SELECT * FROM road where cls_hcm00 = '4'',
-  //        cartocss: '#road {line-width: 2;line-color: #fb8072;line-opacity: 1;}',
-  //        interactivity: ['cls_hcm00', 'direction']
-  //     },
-  //   ]
-  //
-  // }).addTo(map)
-  //   .on('done', function(layer) {
-  //     // Set interactivity
-  //     layer.setInteraction(true);
-  //     // Set up event
-  //     layer.on('featureClick',function(e, latlng, pos, data) {
-  //       console.log(data);
-  //     });
-  //   }).on('error', function() {
-  //     console.log('some error occurred');
-  // });
 });
